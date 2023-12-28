776ffa35efb3509862960b4383d556cd20587104
subhashraaj <150801940+subhashraaj@users.noreply.github.com>
2023-12-28 10:08:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+let str="dodo";
+let ans={};
+for (let i=0;i<=str.length;i++){
+	if(ans(str[i])){
+		ans[str[i]].push(i)
+	}
+	else{
+		ans[str[i]]=[i]
+	}
+}
+console.log(ans)
